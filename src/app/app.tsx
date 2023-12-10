9503cd42280740f4519af31eea9aec68339862a0
Roman Riezanov <romark6@gmail.com>
2023-12-11 00:14:53 +0200

    general-styles Removed testing functionality

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,19 +2,7 @@ import React, { useContext } from "react";
 import { errorContext } from "../core/error/error.provider";
 
 function App() {
-  const { bug, success, info } = useContext(errorContext);
-
-  return (
-    <div>
-      <button
-        onClick={() => {
-          bug("DSADSADA");
-        }}
-      >
-        CLICK
-      </button>
-    </div>
-  );
+  return <div>APP</div>;
 }
 
 export default App;
